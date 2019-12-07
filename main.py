72fc82a211fccca7c09d90c06c637ef440c72356
Tim Hogarty <themaskedtim@gmail.com>
2019-12-06 21:43:05 -0800

    Add initial boilerplate for handling socket connections on port 11211

---
+++ main.py
@@ -0,0 +1,37 @@
+import socketserver
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        close = False
+        while not close:
+            self.data = self.rfile.readline().strip()
+            if not self.data:
+                # EOF, client closed, just return
+                return
+            print("{} wrote:".format(self.client_address[0]))
+            print(self.data)
+            # Likewise, self.wfile is a file-like object used to write back
+            # to the client
+            self.data += b'\n'
+            self.wfile.write(self.data.upper())
+            if b'quit' in self.data:
+                close = True
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 11211
+
+    # Create the server, binding to localhost on port 11211
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
