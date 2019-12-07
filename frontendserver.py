73f8eaeb69eee78392fb633a057513abc609fd08
Tim Hogarty <themaskedtim@gmail.com>
2019-12-06 22:40:13 -0800

    Add simple Flask server and subprocess manager

---
+++ frontendserver.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Use flask to serve the static html file instead of using the python simple server.
+# Gives control of what is actually served from the server, and allows us to do some
+# preprocessing to the html file if we chose choose before it's delivered.
+from flask import Flask
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    message = "Hello, World"
+    return render_template('monitoring_page.html', message=message)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-import socketserver
+#!/usr/bin/env python3
 
-class MyTCPHandler(socketserver.StreamRequestHandler):
-    """
-    The request handler class for our server.
+# Running both servers in the same process
+import subprocess
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+# Handle the task of getting the absolute path for the subprocesses
+import glob
+import os.path
 
-    def handle(self):
-        # self.rfile is a file-like object created by the handler;
-        # we can now use e.g. readline() instead of raw recv() calls
-        close = False
-        while not close:
-            self.data = self.rfile.readline().strip()
-            if not self.data:
-                # EOF, client closed, just return
-                return
-            print("{} wrote:".format(self.client_address[0]))
-            print(self.data)
-            # Likewise, self.wfile is a file-like object used to write back
-            # to the client
-            self.data += b'\n'
-            self.wfile.write(self.data.upper())
-            if b'quit' in self.data:
-                close = True
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 11211
+if __name__ == '__main__':
+    cwd = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
-    # Create the server, binding to localhost on port 11211
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        server.serve_forever()
+    try:
+        subprocess.Popen(('python3',
+            'frontendserver.py'), cwd=cwd)
+    except IndexError:
+        print('Cannot find frontendserver.py')
+        sys.exit(1)
+
+    try:
+        subprocess.Popen(('python3',
+            'memcachedserver.py'), cwd=cwd)
+    except IndexError:
+        print('Cannot find memcachedserver.py')
+        sys.exit(1)
+
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print('All servers terminated')
