ccde7a826c5c466236537b97f8dc2549090c0e2d
Tim Hogarty <themaskedtim@gmail.com>
2019-12-09 19:44:51 -0800

    Switch asyncio streams to protocol for ease of use and speed

--- memcachedserver.py
+++ memcachedserver.py
@@ -3,31 +3,34 @@
 # Manage socket connections for telnet to use the memcached server
 import asyncio
 
-async def handleCommand(reader, writer):
-    while True:
-        try:
-            data = await reader.readline()
-            print("received: ", data)
-            if b'quit' in data:
-                break
-            writer.write(data + b'\r\n')
-            await writer.drain()
-        except:
-            print('Client closed connection without sending \'quit\' command')
-            break
+class MemcachedServer(asyncio.Protocol):
 
+    TIMEOUT = 10 # Seconds
 
-    writer.close()
+    def __init__(self):
+        loop = asyncio.get_running_loop()
+        self.timeout_handle = loop.call_later(self.TIMEOUT, self._timeout)
 
-async def main():
-    server = await asyncio.start_server(
-    handleCommand, '127.0.0.1', 11211)
+    def connection_made(self, transport):
+        self.transport = transport
 
-    addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr}')
+    def data_received(self, data):
+        self.timeout_handle.cancel()
+        self.data = data
+        self.parseReceivedData()
+        self.transport.write(data)
 
+    def parseReceivedData(self):
+        print(b'received: ' + self.data)
+
+    def _timeout(self):
+        self.transport.close()
+
+async def main(host, port):
+    loop = asyncio.get_running_loop()
+    server = await loop.create_server(MemcachedServer, host, port)
     async with server:
         await server.serve_forever()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main('127.0.0.1', 11211))
