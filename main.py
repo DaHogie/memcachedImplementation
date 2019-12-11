cec506c131da4409631d73f9bda3d70cb6b6832c
Tim Hogarty <themaskedtim@gmail.com>
2019-12-11 15:29:11 -0800

    Add sqlite table creation to main.py

--- main.py
+++ main.py
@@ -7,9 +7,67 @@ import subprocess
 import glob
 import os.path
 
+# Handle command line arguments
+import argparse
 
-if __name__ == '__main__':
+# Sqlite
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def main():
+    parser = argparse.ArgumentParser(description='Start the memcached and front end servers')
+    parser.add_argument('database', metavar='database', type=str,
+                        help='the database for the memcached server')
+
+    args = parser.parse_args()
+
     cwd = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    database = cwd+'/'+args.database
+
+    sql_create_keys_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                    key text PRIMARY KEY,
+                                    flags integer NOT NULL,
+                                    bytes integer,
+                                    dataBlock text
+                                ); """
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create keys table
+        create_table(conn, sql_create_keys_table)
+    else:
+        print("Error! cannot create the database connection.")
+
 
     try:
         subprocess.Popen(('python3',
@@ -31,3 +89,6 @@ if __name__ == '__main__':
             pass
     except KeyboardInterrupt:
         print('All servers terminated')
+
+if __name__ == '__main__':
+    main()
